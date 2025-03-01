90c2b4d83f41be7f89149a12bd221c42dd5360d2
zoyirr <zoyir6171@gmail.com>
2025-03-01 17:44:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// let son=+prompt("son kiriting")
+
+
+
+
+
+// if(son=1){
+//     alert("yomon")
+// }else if(son==2){
+//     alert("qoniqarsiz")
+// }
+
+
+
+
+// let mashina={
+//     nomi: "Jentra",
+//     yili: "2025",
+//     rangi: "qora",
+//     probeg: "10 km",
+//     egasi:{
+//         ism: "Zoyirjon",
+//         familiyasi: "ruziqulov"
+//     }
+// }
+
+
+
+// console.log(mashina);
+
+
+
+
+const person={
+    ism:null,
+    id:null,
+    parol:null
+}
+
+
+console.log(person);
+person.ism=prompt("ism kiriting");
+
+
+
+person.id=prompt("id kiriting");
+
+
+person.parol=prompt("parol kiriting");
+console.log(person);
+
